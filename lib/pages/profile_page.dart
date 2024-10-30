ded510e9973a0f4d18c0b2cfd8e78ee7f87a44a7
Arzuza <arzuline13@gmail.com>
2024-10-30 21:18:46 +0700

    Initial commit

---
+++ lib/pages/profile_page.dart
@@ -0,0 +1,194 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          children: [
+            Container(
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                          onPressed: () {
+                            print("setting");
+                          },
+                          icon: const Icon(Icons.settings)),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      CircleAvatar(
+                        radius: 100,
+                        backgroundColor: Colors.orange.shade500,
+                        backgroundImage: const AssetImage(
+                          'assets/profile.jpg',
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      Text(
+                        'Fikri Ardiansyah',
+                        style:
+                            Theme.of(context).textTheme.headlineSmall!.copyWith(
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.black,
+                                ),
+                      ),
+                      const Text(
+                        'Faculty of Mathematics and Computer Science',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                        ),
+                      ),
+                      const Text(
+                        'Informatics',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                        ),
+                      ),
+                      const Text(
+                        'Angkatan 2021',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 25),
+            Container(
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.white,
+                boxShadow: const [
+                  BoxShadow(
+                    color: Color.fromRGBO(0, 0, 0, 0.1),
+                    blurRadius: 12,
+                    spreadRadius: 0,
+                    offset: Offset(
+                      0,
+                      4,
+                    ),
+                  ),
+                ],
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Details',
+                    style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w700,
+                        ),
+                  ),
+                  const SizedBox(height: 25),
+                  Row(
+                    children: [
+                      Text(
+                        'NIM :',
+                        style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                              color: Colors.black87,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w700,
+                            ),
+                      ),
+                      Text(
+                        ' 212225001',
+                        style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                              color: Colors.black54,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                            ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 15),
+                  Row(
+                    children: [
+                      Text(
+                        'Phone :',
+                        style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                              color: Colors.black87,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w700,
+                            ),
+                      ),
+                      Text(
+                        '+62 812 3345 0424',
+                        style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                              color: Colors.black54,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                            ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 15),
+                  Row(
+                    children: [
+                      Text(
+                        'Email :',
+                        style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                              color: Colors.black87,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w700,
+                            ),
+                      ),
+                      Text(
+                        ' arsyahns13@gmail.com',
+                        style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                              color: Colors.black54,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                            ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 15),
+                  Row(
+                    children: [
+                      Text(
+                        'Address :',
+                        style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                              color: Colors.black87,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w700,
+                            ),
+                      ),
+                      Text(
+                        ' Kebumen',
+                        style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                              color: Colors.black54,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                            ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
